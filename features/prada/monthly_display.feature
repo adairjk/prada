42c02830520927e0a90c842579d948af3d5a015e
Jarod Adair <jarod.adair@manheim.com>
2013-09-28 14:12:59 -0500

    add step to check shoe description

--- features/prada/monthly_display.feature
+++ features/prada/monthly_display.feature
@@ -4,9 +4,9 @@ Feature: Monthly display of new releases
   As a user of the Shoe store
   I want to be able to visit a link for each month and see the shoes being released
 
-  Scenario:Month view displays a small Blurb of each shoe
+  Scenario:Month view displays a description of each shoe
     When  I select a month
-    Then I should see information about each shoe for that month
+    Then I should see a description of each shoe for that month
 
   Scenario: Month view displays an image of each shoe being released for that month
     When I select a month
--- features/step_definitions/shoe_store_steps.rb
+++ features/step_definitions/shoe_store_steps.rb
@@ -4,9 +4,13 @@ When /^I select a month$/ do
 end
 
 Then /^I should see pricing information on each of the shoes$/ do
-  @prada.search_results_page.shoes_pricing.each { |price| price.text.should_not be_nil}
+  @prada.search_results_page.shoe_rows.each { |row| row.td(:class => "shoe_price").text.should_not be_nil }
 end
 
 Then /^I should see an image displayed for each shoe?/ do
-  @prada.search_results_page.shoe_rows.each { |row| row.image.should be_present}
+  @prada.search_results_page.shoe_rows.each { |row| row.image.should be_present }
+end
+
+Then /^I should see a description of each shoe for that month$/ do
+  @prada.search_results_page.shoe_rows.each { |row| row.td(:class => "shoe_description").text.should_not be_nil }
 end
